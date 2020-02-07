6e665b87ba3227875d8bc13273209738ad6f79a8
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-02-07 10:35:07 +0300

    Rename and simplify map component

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import DeckGL from '@deck.gl/react';
 import { StaticMap } from 'react-map-gl';
-import { HexagonLayer } from '@deck.gl/aggregation-layers';
 import { ScatterplotLayer } from '@deck.gl/layers';
 import { HeatmapLayer } from '@deck.gl/aggregation-layers';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-import assaultData from '../data/assault2018.json';
-import autoTheftData from '../data/auto-theft2018.json';
-import breakAndEnterData from '../data/break-and-enter2018.json';
-import homicideData from '../data/homicide2018.json';
-import robberyData from '../data/robbery2018.json';
-import theftOverData from '../data/theft-over2018.json';
-
-function MyMap() {
+function MapComponent({ crimeData }) {
   const MAPBOX_ACCESS_TOKEN =
     'pk.eyJ1IjoibWFycy1nYWJkdWxvdiIsImEiOiJjazZhb2t1eGgwNnVmM2xvZzUzbnpndHh1In0.268onGjsZnUr295iWoZzag';
 
   // Initial viewport settings
   const initialViewState = {
-    longitude: -79.407884,
-    latitude: 43.724132,
-    zoom: 11,
+    longitude: -79.297986,
+    latitude: 43.710172,
+    zoom: 10.5,
     pitch: 0,
     bearing: 0
   };
 
-  const layers = [
-    new ScatterplotLayer({
+  function scatterplotLayer(data) {
+    return new ScatterplotLayer({
       id: 'scatter',
-      data: robberyData,
+      data: data,
       opacity: 0.8,
       filled: true,
       radiusMinPixels: 2,
@@ -36,17 +29,32 @@ function MyMap() {
       getPosition: d => [d.long, d.lat],
       getFillColor: d =>
         d.premisetype === 'Outside' ? [200, 0, 40, 150] : [255, 140, 0, 100]
-    })
-  ];
+    });
+  }
+
+  function heatmapLayer(data) {
+    return new HeatmapLayer({
+      id: 'heat',
+      data: data,
+      getPosition: d => [d.long, d.lat],
+      getWeight: 60,
+      radiusPixels: 60
+    });
+  }
+
+  const layers = [scatterplotLayer(crimeData), heatmapLayer(crimeData)];
   return (
     <DeckGL
       initialViewState={initialViewState}
       controller={true}
       layers={layers}
     >
-      <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+      <StaticMap
+        mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+        mapStyle='mapbox://styles/mapbox/dark-v8'
+      />
     </DeckGL>
   );
 }
 
-export default MyMap;
+export default MapComponent;
