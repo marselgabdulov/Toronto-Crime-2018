997f3ce78f74e29aabc8402aba2ddc80cec6c9fa
Marsel Gabdulov <marsel.gabdulov@gmail.com>
2020-02-08 17:00:05 +0300

    Remove google api key

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ function App() {
           <MapComponent
             crimeData={currentResult}
             currentCrimeName={currentCrimeName}
-            googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyBvjINMXRM7jTpqM80B2drht5CnaVfjcX0&callback=initMap'
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `100vh` }} />}
             mapElement={<div style={{ height: `100%` }} />}
